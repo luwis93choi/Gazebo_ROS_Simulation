d1414013d3d351f46d0f13d753fb39d9494f5cda
luwis93choi <luwis93choi@hotmail.com>
2021-04-13 00:48:58 +0900

    Added launch for current model and world

--- ROS2_Gazebo/gazebo_practice_04_rospkg_model_control/setup.py
+++ ROS2_Gazebo/gazebo_practice_04_rospkg_model_control/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import setup
 
 package_name = 'gazebo_practice_04_rospkg_model_control'
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
